d6d757d958b1a3b6ebeac663b8549730e6c7b4d9
Vee-Guz <vero.guzman48612@gmail.com>
2021-07-24 13:50:33 -0700

    updated deploy script

--- Linux/deploy.sh
+++ Linux/deploy.sh
@@ -6,9 +6,12 @@ cp ~/DevopsSecret/veroguz.tfvars.asc .
 #Decrypt key
 gpg --decrypt --no-symkey-cache --output veroguz.tfvars veroguz.tfvars.asc
 
-#deploy terraform
+#excute and start terraform
 terraform plan
 terraform apply
+
+#wait 60s
+sleep 60s
 
 #remove encrpted and decrypted keys from repository
 rm veroguz.tfvars.asc veroguz.tfvars
